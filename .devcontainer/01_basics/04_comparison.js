fb74dec74a89f1bd154dc584f9be340d9c373046
kritikaIS <145431102+kritikaIS@users.noreply.github.com>
2025-02-09 13:53:26 +0000

    comparisons

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2<=1)
+
+// console.log("2">1); //will not give the desired predictable result always 
+// console.log("02">1);
+
+//console.log(null>0) //false
+//console.log(null==0)    //false
+//console.log(null>=0)    //true b/c the equality check == and comparisons work differently
+                        //comparison convert null to number => 0 
+                        // that's why null>=0 true but null >0 is false
+// console.log(undefined==0)   //false
+// console.log(undefined>=0)   //false
+// console.log(undefined<=0)   //false
+
+
