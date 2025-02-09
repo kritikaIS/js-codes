6c9eeae06ab194fca76b58688322ba5675039eef
kritikaIS <145431102+kritikaIS@users.noreply.github.com>
2025-02-09 12:58:03 +0000

    conversion in js

---
+++ .devcontainer/01_basics/03_conversion_operationns.js
@@ -0,0 +1,34 @@
+// let score="33abc";
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let value_num=Number(score)
+// console.log(typeof value_num)
+// console.log(value_num) 
+//NaN not a number
+//if score is null then conversion to number is 0
+//if score is undefined then conversion to number is NaN 
+// if boolean ie if true->1 and if false->0
+
+
+
+// NOTES
+//"33"=> 33
+// "33abc" => NaN ,typeof(NaN)=>number
+// true => 1 ;false->0
+
+let isLoggedIn="abc";
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// NOTES
+// 1 or any number=> true
+// 0 => false
+// "" => false
+// "kritika" => true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
