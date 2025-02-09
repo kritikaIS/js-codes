675a8d93d20fd0e9be5d8b254a3443bd4bf02885
kritikaIS <145431102+kritikaIS@users.noreply.github.com>
2025-02-09 11:55:27 +0000

    datatypes in js

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+let name="kritika"
+let age=18
+let isLoggedIn=true
+
+//number
+//bigint
+//string=> ""
+//boolean=> true/false
+//null - standalone value
+//undefined =>
+//symbol=> unique
+
+//object
+
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined)   //undefined
